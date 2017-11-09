99d296d174e92bc8bcf1af4ee25300f73833da55
Bartłomiej Mucha <bartek.mucha@dogdigital.com>
2017-11-09 07:35:01 +0100

    First commit

---
+++ src/Quantus.Sitecore/Repositories/PluralProviderRepository.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.Configuration;
+using System.Configuration.Provider;
+using Quantus.Providers;
+using Sitecore.Configuration;
+using Sitecore.Configuration.Providers;
+
+namespace Quantus.Sitecore.Repositories
+{
+    public static class PluralProviderRepository
+    {
+        private static IEnumerable<ProviderBase> _all;
+        private static IPluralProvider _defaultPluralProvider;
+
+        public static IEnumerable<ProviderBase> All
+        {
+            get
+            {
+                if (_all == null)
+                {
+                    Initialize();
+                }
+
+                return _all;
+            }
+        }
+
+        public static IPluralProvider DefaultPluralProvider
+        {
+            get
+            {
+                if (_defaultPluralProvider == null)
+                {
+                    Initialize();
+                }
+
+                return _defaultPluralProvider;
+            }
+        }
+
+        private static void Initialize()
+        {
+            var providers = Factory.GetProviders<ProviderBase, ProviderCollectionBase<ProviderBase>>("quantus", out var defaultProvider);
+            if (!(defaultProvider is IPluralProvider defaultSearchResultFormatter))
+            {
+                throw new ConfigurationErrorsException("The default quantus provider must derive from IPluralProvider");
+            }
+
+            _all = providers;
+            _defaultPluralProvider = defaultSearchResultFormatter;
+        }
+    }
+}
---
+++ src/Quantus.Sitecore/Services/PluralService.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+using Quantus.Providers;
+using Quantus.Sitecore.Repositories;
+
+namespace Quantus.Sitecore.Services
+{
+    public static class PluralService
+    {
+        public static PluralCategory GetPluralCategory(string language, decimal quantity)
+        {
+            var provider = PluralProviderRepository.All.FirstOrDefault(x => x.Name == language) as IPluralProvider ?? PluralProviderRepository.DefaultPluralProvider;
+
+            return provider.GetPluralCategory(quantity);
+        }
+    }
+}
